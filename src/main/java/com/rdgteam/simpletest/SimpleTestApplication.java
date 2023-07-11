be450b717c4a6bbd488a50a1fee9e888c5cc23ef
Renan Daniel <renan.daniel@gmail.com>
2023-07-11 13:34:52 -0300

    Testing

---
+++ src/main/java/com/rdgteam/simpletest/SimpleTestApplication.java
@@ -0,0 +1,13 @@
+package com.rdgteam.simpletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdgteam/simpletest/SimpleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdgteam.simpletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
